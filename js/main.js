6e9d3e130d314c030b03c1a89cab0e689d43ed2e
Devang <devngc@gmail.com>
2022-10-22 08:57:29 -0400

    fix(*): add weather

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ const bodyEl = document.body;
 const coinContainerEl = document.getElementById("coin-container");
 const coinInfoEl = document.getElementById("coin-info");
 const timeEl = document.getElementById("time");
+const weatherEl = document.getElementById("weather-container");
+const weatherInfoEl = document.getElementById("weather-info");
+const weatherLocationEl = document.getElementById("weather-location");
 const authorEl = document.getElementById("author");
 const defaultBackground = './images/gradient.jpg';
 
@@ -91,3 +94,53 @@ function updateTime() {
 
 setInterval(updateTime, 60);
 
+
+function updateWeather(data) {
+    const city = data.name;
+    const temperature = data.main.temp;
+    const icon = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    weatherInfoEl.innerHTML = `
+        <img src=${icon}>
+        <p>${Math.round(temperature)}°C</p>
+    `;
+    weatherLocationEl.innerHTML = `
+        <p>${city}</p>
+    `;
+}
+
+function showWeather(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error('Could not get weather data');
+            }
+            return res.json();
+        })
+        .then(data => updateWeather(data))
+        .catch(err => console.error(err));
+}
+
+function handleWeatherError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            weatherEl.innerHTML = '<p>Permission denied</p>';
+            break;
+        case error.POSITION_UNAVAILABLE:
+            weatherEl.innerHTML = '<p>Position unavailable</p>';
+            break;
+        case error.TIMEOUT:
+            weatherEl.innerHTML = '<p>Request timeout</p>';
+            break;
+        case error.UNKNOWN_ERROR:
+            weatherEl.innerHTML = '<p>Unknown error</p>';
+            break;
+    }
+}
+
+
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showWeather, handleWeatherError);
+}
